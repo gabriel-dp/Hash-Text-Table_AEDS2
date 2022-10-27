5512f27b152baa9e6725f5dc9d3127007aa2ebf1
Gabriel de Paula <gabriel.meira.2004@gmail.com>
2022-10-27 13:56:37 -0300

    ♻️ Refactored and commented files

---
+++ hash.h
@@ -0,0 +1,27 @@
+#ifndef HASH_H
+
+#define HASH_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define SLOTS 509
+#define MAX_WORD_LENGTH 20
+
+typedef struct occurrence {
+    int line;
+    struct occurrence* p_prox;
+} occurrence_t;
+
+typedef struct {
+    char word[MAX_WORD_LENGTH + 1];
+    int quantity;
+    occurrence_t* first;
+} hash;
+
+hash* createHashTable();
+void insertElement(hash*, char*, int);
+void printOccurrences(hash*, char*); 
+
+#endif
